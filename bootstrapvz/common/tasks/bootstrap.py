50c54afdbcedeee54cdb6deca188790d51c91d3e
Marcin Engelmann <mengelmann@octivi.com>
2018-01-23 10:06:51 +0100

    Cosmetic - debootstrap creates tarball that is actually gzipped tar, so change extension

--- bootstrapvz/common/tasks/bootstrap.py
+++ bootstrapvz/common/tasks/bootstrap.py
@@ -38,7 +38,7 @@ def get_tarball_filename(info):
     # Filter info.root which points at /target/volume-id, we won't ever hit anything with that in there.
     hash_args = [arg for arg in arguments if arg != info.root]
     tarball_id = sha1(repr(frozenset(options + hash_args))).hexdigest()[0:8]
-    tarball_filename = 'debootstrap-' + tarball_id + '.tar'
+    tarball_filename = 'debootstrap-' + tarball_id + '.tgz'
     return os.path.join(os.path.abspath(info.workspace_root), tarball_filename)
 
 
